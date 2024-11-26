4a1d1581f7fd1547a3f20b304775476e163bfdc2
Hirofumi Omi <hirofumi.omi@3-shake.com>
2024-11-26 14:11:57 +0900

    🎧

--- lvim/config.lua
+++ lvim/config.lua
@@ -227,4 +227,12 @@ lvim.plugins = {
             end, { expr = true })
         end,
     },
+    {
+        url = "https://github.com/atusy/treemonkey.nvim",
+        init = function()
+            vim.keymap.set({ "x", "o" }, "m", function()
+                require("treemonkey").select({ ignore_injections = false })
+            end)
+        end
+    }
 }
