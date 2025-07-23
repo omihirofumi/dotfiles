e8e9fa4a326302d142c6af058b212bae8bf83d8f
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2025-07-23 17:22:10 +0900

    Update home/.gitconfig Update home/.zshrc Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/config/keymaps.lua

--- home/private_dot_config/nvim/lua/config/keymaps.lua
+++ home/private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
+-- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 local set = vim.keymap.set
 local del = vim.keymap.del
+local permalink = require("util.permalink")
 
 set({ "v", "n" }, "y", '"+y')
 set({ "n" }, "Y", '"+y$')
@@ -16,47 +19,21 @@ end, { desc = "Delete Buffer" })
 set("n", "<C-w><C-c>", function()
   Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
-
 set("n", "<leader>xx", "<Cmd>source %<CR>", { desc = "source %" })
-
 set("n", "<leader>by", ':let @+ = fnamemodify(expand("%"), ":.")<CR>', { desc = "Copy file path" })
-
 del("n", "<C-s>")
 
 vim.api.nvim_create_user_command("RenameIdentifier", function()
   vim.lsp.buf.rename()
 end, {})
 
--- GitHubを開く
-set("n", "<leader>gho", function()
-  vim.fn.system("gh browse")
-end, { desc = "Open github in browser" })
+-- GitHub関連（関数を外部ファイルから読込）
+set("n", "<leader>gho", permalink.open_github, { desc = "Open github in browser" })
+set("n", "<leader>ghf", permalink.open_github_file, { desc = "Open current file in GitHub" })
+set("n", "<leader>ghl", permalink.open_github_line, { desc = "Open current line in GitHub" })
+set("v", "<leader>ghl", permalink.open_github_range, { desc = "Open selected range in GitHub" })
 
--- ファイル全体をGitHubで開く
-set("n", "<leader>ghf", function()
-  local file_path = vim.fn.expand("%")
-  local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("\n", "")
-
-  local cmd = string.format("gh browse --branch %s %s", branch, file_path)
-  vim.fn.system(cmd)
-end, { desc = "Open current file in GitHub" })
-
--- カーソル行をGitHubで開く
-set("n", "<leader>ghl", function()
-  local line_num = vim.fn.line(".")
-  local file_path = vim.fn.expand("%")
-  local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("\n", "")
-
-  local cmd = string.format("gh browse --branch %s %s:%d", branch, file_path, line_num)
-  vim.fn.system(cmd)
-end, { desc = "Open current line in GitHub" })
-
--- 選択範囲をGitHubで開く
-set("v", "<leader>ghl", function()
-  local start_line = vim.fn.line("'<")
-  local end_line = vim.fn.line("'>")
-  local file_path = vim.fn.expand("%")
-  local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("\n", "")
-  local cmd = string.format("gh browse --branch %s %s:%d-%d", branch, file_path, start_line, end_line)
-  vim.fn.system(cmd)
-end, { desc = "Open selected range in GitHub" })
+-- パーマリンク生成（関数を外部ファイルから読込）
+set({ "n", "v" }, "<leader>cP", permalink.copy_relative_permalink, {
+  desc = "Copy relative permalink for prompt",
+})
