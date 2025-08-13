cd88b80a76f9a63712c41014a5737cd583af57a6
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2025-08-13 12:13:29 +0900

    Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/pr-review.lua

---
+++ home/private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
+    keys = {
+      { "<leader>gB", ":Gitsigns change_base origin/main<CR>", desc = "Gitsigns: Compare with main" },
+      { "<leader>gR", ":Gitsigns reset_base<CR>", desc = "Gitsigns: Reset base (worktree)" },
+    },
+  },
+}
