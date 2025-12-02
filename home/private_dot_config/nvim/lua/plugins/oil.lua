dc5ad0c78d0f6d7effd2c7764417d88f80e95f3c
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2025-12-02 13:49:06 +0900

    Update .config/nvim/lua/plugins/oil.lua

--- home/private_dot_config/nvim/lua/plugins/oil.lua
+++ home/private_dot_config/nvim/lua/plugins/oil.lua
@@ -1,18 +1,34 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  init = function()
+    local group = vim.api.nvim_create_augroup("oil_start_dir", { clear = true })
+    vim.api.nvim_create_autocmd("VimEnter", {
+      group = group,
+      callback = function()
+        if vim.fn.argc() ~= 1 then
+          return
+        end
+        local arg = vim.fn.argv(0)
+        if arg and vim.fn.isdirectory(arg) == 1 then
+          vim.cmd.cd(arg)
+          require("oil").open(arg, { preview = {} })
+        end
+      end,
+    })
+  end,
   keys = {
     {
       "<leader>e",
       function()
-        require("oil").open_float()
+        require("oil").open_float(nil, { preview = {} })
       end,
       desc = "Open parent directory (float)",
     },
     {
       "-",
       function()
-        require("oil").open_float()
+        require("oil").open_float(nil, { preview = {} })
       end,
       desc = "Open parent directory (float)",
     },
@@ -23,6 +39,9 @@ return {
     keymaps = {
       ["q"] = "actions.close",
       ["<esc>"] = "actions.close",
+    },
+    preview_win = {
+      update_on_cursor_moved = true,
     },
     float = {
       padding = 2,
